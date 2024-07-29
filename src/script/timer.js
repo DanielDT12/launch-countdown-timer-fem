1676057563522676b9422ce737bdfa26c4005751
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-07-29 07:19:58 +0200

    Bygget timer funksjon

--- src/script/timer.js
+++ src/script/timer.js
@@ -1,11 +1,28 @@
-const seconds = document.querySelector("#js-seconds");
-const minutes = document.querySelector("#js-minutes");
-const hours = document.querySelector("#js-hours");
-const days = document.querySelector("#js-days");
-Ì;
-
 export function countDownTimer() {
-	const countDownDate = new Date("Okt 5, 2024 20:37:35").getTime();
+	const countDownDate = new Date("Oct 5, 2024 20:37:35").getTime();
 
-	const countdownFunction = setInterval(function () {});
+	console.log(countDownDate);
+
+	const countdownFunction = setInterval(function () {
+		const secondsText = document.querySelector("#js-seconds");
+		const minutesText = document.querySelector("#js-minutes");
+		const hoursText = document.querySelector("#js-hours");
+		const daysText = document.querySelector("#js-days");
+
+		const now = new Date().getTime();
+
+		const distance = countDownDate - now;
+
+		const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		const hours = Math.floor(
+			(distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+		);
+		const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+		daysText.textContent = days.toString();
+		hoursText.textContent = hours.toString();
+		minutesText.textContent = minutes.toString();
+		secondsText.textContent = seconds.toString();
+	});
 }
