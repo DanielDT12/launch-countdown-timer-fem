414015303ff6f15c6902a0988eae677b425a688e
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-07-29 06:39:37 +0200

    startet javascript timer

---
+++ src/script/timer.js
@@ -0,0 +1,11 @@
+const seconds = document.querySelector("#js-seconds");
+const minutes = document.querySelector("#js-minutes");
+const hours = document.querySelector("#js-hours");
+const days = document.querySelector("#js-days");
+Ì;
+
+export function countDownTimer() {
+	const countDownDate = new Date("Okt 5, 2024 20:37:35").getTime();
+
+	const countdownFunction = setInterval(function () {});
+}
